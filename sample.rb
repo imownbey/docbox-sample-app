c0456e543ac6c7c662f9b027650d8c8da01b5801
Ian Ownbey <imownbey@gmail.com>
2008-11-06 17:08:48 -0500

    Initial

---
+++ sample.rb
@@ -0,0 +1,28 @@
+# Spam.
+
+class FirstClass
+  # This is just a method.
+  #
+  # And this is rubycon!
+  def some_method(params_yup)
+    p "Hello World!"
+  end
+  
+  private
+  
+  def private_method
+  end
+end
+
+# That was filler. 
+# Just for testing purpose.
+class SecondClass
+
+  # This is a long multi line comment
+  # 
+  # 
+  # And I love it.
+  def some_method(params_yup)
+    p "Another, Hello!"
+  end
+end
